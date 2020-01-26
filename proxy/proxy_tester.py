f4fab47fc34e20e57fbd93fd913d240f9b8f204a
henry <tahafut@gmail.com>
2020-01-26 13:54:04 -0800

    pass all other than keep-alive

--- proxy/proxy_tester.py
+++ proxy/proxy_tester.py
@@ -47,7 +47,7 @@ class HttpRequestTest(BaseTest):
             self.assertDictEqual(response, {
                 'Accept-Encoding': 'identity',
                 'Host': 'localhost:8000',
-                'User-Agent': 'Python-urllib/3.6'
+                'User-Agent': 'Python-urllib/3.7'
             })
 
 
@@ -63,7 +63,7 @@ class KeepAliveTest(BaseTest):
             self.assertDictEqual(response, {
                 'Accept-Encoding': 'identity',
                 'Host': 'localhost:8000',
-                'User-Agent': 'Python-urllib/3.6',
+                'User-Agent': 'Python-urllib/3.7',
                 'Connection': 'Keep-Alive'
             })
 
